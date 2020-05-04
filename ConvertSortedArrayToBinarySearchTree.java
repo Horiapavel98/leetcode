9821ee7fa7206c52057bc5ba9c4e74064025540c
Horia Pavel <horiapavel@yahoo.com>
2020-05-04 09:49:38 +0300

    Update leetcode problem

--- ConvertSortedArrayToBinarySearchTree.java
+++ ConvertSortedArrayToBinarySearchTree.java
@@ -19,32 +19,48 @@ public class ConvertSortedArrayToBinarySearchTree {
     }
 
     private static int[] getRight(int[] nums) {
-        int[] toReturn = new int[nums.length/2];
         if (nums.length % 2 != 0) {
-                
+            int[] toReturn = new int[nums.length/2];
             for (int i = 1 ; i <= nums.length/2 ; ++i) {
                 toReturn[i - 1] = nums[nums.length/2 + i];
             }
+            
+            return toReturn;
         }
         else {
-            
+            int[] toReturn = new int[nums.length/2 - 1];
             for (int i = 1 ; i < nums.length/2 ; ++i) {
                 toReturn[i - 1] = nums[nums.length/2 + i];
             }
+            
+            return toReturn;
         }
-        return toReturn;
     }
 
     private static void recurse(TreeNode node, int[] nums) {
-        // split left
-        int[] left = getLeft(nums);
-        // split right
-        int[] right = getRight(nums);
+        if (nums.length == 3){
+            node.left = new TreeNode(nums[0]);
+            node.right = new TreeNode(nums[2]);
+            return;
+        }else if (nums.length == 2) {
+            node.left = new TreeNode(nums[0]);
+            return;
+        }else {
+            // split left
+            int[] left = getLeft(nums);
+            // split right
+            int[] right = getRight(nums);
+            
+            if (left.length != 0) {
+                node.left = new TreeNode(left[left.length/2]);
+                recurse(node.left, left);
+            }
 
-        node = new TreeNode(nums[nums.length/2]);
-
-        recurse(node.left, left);
-        recurse(node.right, right);
+            if (right.length != 0) {
+                node.right = new TreeNode(right[right.length/2]);
+                recurse(node.right, right);
+            }
+        }
     }
 
     public static TreeNode sortedArrayToBST(int[] nums) {
@@ -52,14 +68,21 @@ public class ConvertSortedArrayToBinarySearchTree {
             return null;
         }
 
-        TreeNode root = null;
-        recurse(root, nums);
+        TreeNode root = new TreeNode(nums[nums.length/2]);
+        
+        if (nums.length == 1) {
+            return root;
+        }else { 
+            recurse(root, nums);
+        }
 
-        return null;
+        return root;
     }
 
     public static void main(String[] args) {
-        int[] arr = new int[] {-10, -3, 0, 5, 9};
-        sortedArrayToBST(arr);
+        int[] arr = new int[] {-1, 0, 1, 2};
+        TreeNode root = sortedArrayToBST(arr);
+
+        System.out.println("Done..");
     }
 }
