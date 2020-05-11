28cb4f872e98de668b2620dff04f3755333c717c
Horia Pavel <horiapavel@yahoo.com>
2020-05-11 21:35:57 +0300

    Add code for climbing stairs, not completed

---
+++ ClimbingStairs.py
@@ -0,0 +1,43 @@
+from typing import List
+
+"""
+Count all the possible ways of climbing stairs
+using either 1 or 2 steps at a time.
+"""
+class ClimbingStairs:
+    
+    # Climbing stairs recursive method
+    # Initial method does not work
+    # Requires memorisation
+    """
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        else:
+            return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+
+    """
+
+    def recursiveHelper(self, n: int, mem) -> int:
+        if n == 1:
+            mem.append(1)
+            return 1
+        elif n == 2:
+            mem.append(2)
+            return 2
+        elif n < len(mem):
+            return mem[n]
+        else:
+            tmp = self.recursiveHelper(n - 1, mem) + self.recursiveHelper(n - 2, mem)
+            mem.append(tmp)
+            return tmp
+
+    def climbStairs(self, n: int) -> int:
+        return self.recursiveHelper(n, list())
+
+
+
+obj = ClimbingStairs()
+print(obj.climbStairs(5))
